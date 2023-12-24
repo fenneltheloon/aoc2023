e8e8b30e62343bac6b9c7b69e16c6c73a1adbdec
Ethan Meltzer <captainbanaynays@gmail.com>
2023-12-24 03:39:22 -0500

    about to fuck shit up
    saving you if it goes wrong

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,16 @@
+use core::fmt;
 use std::io;
 
+#[derive(Debug, Clone)]
+struct OddError;
+
+impl fmt::Display for OddError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "odd number of elements supplied")
+    }
+}
+
+#[derive(Debug)]
 struct Map {
     dest: usize,
     source: usize,
@@ -27,8 +38,19 @@ impl Map {
         None
     }
 
+    fn rev_map(&self, a: usize) -> Option<usize> {
+        if self.is_rev_mappable(a) {
+            return Some(a - self.dest + self.source);
+        }
+        None
+    }
+
     fn is_mappable(&self, a: usize) -> bool {
         a >= self.source && a < self.source + self.range
+    }
+
+    fn is_rev_mappable(&self, a: usize) -> bool {
+        a >= self.dest && a < self.dest + self.range
     }
 }
 
@@ -43,24 +65,29 @@ fn main() {
     let mut input_iter = input.iter();
     let seeds = input_iter.by_ref().next().unwrap().to_owned();
 
-    let seeds: Vec<&str> = seeds.split_whitespace().skip(1).collect();
-    let mut seeds_int: Vec<usize> = Vec::new();
-
-    for seed in seeds {
-        let seed = seed.parse::<usize>().unwrap();
-        seeds_int.push(seed);
-    }
-
-    let seeds = seeds_int;
+    let mut seeds = seeds.split_whitespace().skip(1).collect::<Vec<_>>();
 
     // Now get each of the different maps and add them to a Vec of Vec of maps
     let mut matrix: Vec<Vec<Map>> = Vec::new();
+
+    seeds.reverse();
+    matrix.push(Vec::new());
+    while !seeds.is_empty() {
+        let mut seed_s = seeds.pop().unwrap().to_string();
+        let seed_r = seeds.pop().unwrap();
+        seed_s.push_str(" ");
+        seed_s.push_str(seed_s.to_owned().as_str());
+        seed_s.push_str(seed_r);
+        matrix[0].push(Map::new(seed_s.as_str()));
+    }
+
+    input_iter.by_ref().next();
 
     for i in 0..7 {
         matrix.push(Vec::new());
         let map_s = input_iter
             .by_ref()
-            .skip(2)
+            .skip(1)
             .take_while(|l| !((**l).trim().is_empty()));
         for map in map_s {
             matrix[i].push(Map::new(map));
@@ -84,4 +111,32 @@ fn main() {
         }
     }
     println!("{min}");
+}
+
+fn pf(matrix: &Vec<Vec<Map>>, n: usize, i: usize) -> usize {
+    match matrix.get(i) {
+        Some(maps) => {
+            for map in maps {
+                if let Some(new) = map.map(n) {
+                    return pf(matrix, new, i + 1);
+                }
+            }
+            return pf(matrix, n, i + 1);
+        }
+        None => return n,
+    }
+}
+
+fn pb(matrix: &Vec<Vec<Map>>, n: usize, i: usize) -> usize {
+    match matrix.get(i) {
+        Some(maps) => {
+            for map in maps {
+                if let Some(new) = map.rev_map(n) {
+                    return pb(matrix, new, i - 1);
+                }
+            }
+            return pb(matrix, n, i - 1);
+        }
+        None => return n,
+    }
 }
